ca6bab6941536257a310806609fb89e98168bee0
Raymond Maung <raymaung@gmail.com>
2016-03-20 23:14:06 +1100

    Add /users/new

--- programming-phoenix/rumbl/web/controllers/user_controller.ex
+++ programming-phoenix/rumbl/web/controllers/user_controller.ex
@@ -1,6 +1,24 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
+  def new(conn, _params) do
+    changeset = Rumbl.User.changeset(%Rumbl.User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{ "user" => user_params}) do
+    changeset = Rumbl.User.changeset(%Rumbl.User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+          |> put_flash(:info, "#{user.name} created!")
+          |> redirect(to: user_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
