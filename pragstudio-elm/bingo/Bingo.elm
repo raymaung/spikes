faaaba4e4c94bba4d6fd1ea25165a857df198c38
Raymond Maung <raymaung@gmail.com>
2016-03-25 15:55:18 +1100

    08-listing-entries

--- pragstudio-elm/bingo/Bingo.elm
+++ pragstudio-elm/bingo/Bingo.elm
@@ -23,8 +23,27 @@ pageFooter =
         [ text "The Pragmatic Studio"]
     ]
 
+entryItem phrase points =
+  li []
+    [
+      span [class "phrase"] [text phrase],
+      span [class "points"] [text (toString points) ]
+    ]
+
+entryList =
+  ul []
+    [
+      entryItem "Future-Proof" 100,
+      entryItem "Doing Agile" 200
+    ]
+
 view =
-  div [ id "container"] [ pageHeader, pageFooter ]
+  div [ id "container"]
+      [
+        pageHeader,
+        entryList,
+        pageFooter
+      ]
 
 main =
   view
