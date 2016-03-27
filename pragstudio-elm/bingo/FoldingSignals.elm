4decfcb6ba4a37a6ad7086b34b03134541533e6c
Raymond Maung <raymaung@gmail.com>
2016-03-27 13:38:09 +1100

    07-model-view-update

--- pragstudio-elm/bingo/FoldingSignals.elm
+++ pragstudio-elm/bingo/FoldingSignals.elm
@@ -2,17 +2,28 @@ import Graphics.Element exposing (..)
 import Keyboard
 import Mouse
 
+type alias Model = Int
 
-keyPressedCount: Signal Int
-keyPressedCount =
-  Signal.foldp (\_ count -> count + 1) 0 Keyboard.presses
+initialModel: Model
+initialModel = 0
 
-mouseClickCount: Signal Int
-mouseClickCount =
-  Signal.foldp (\_ count -> count + 1) 0 Mouse.clicks
+model: Signal Model
+model =
+  Signal.foldp update initialModel Mouse.clicks
 
+
+-- UPDATE
+update: a -> Model -> Model
+update event model =
+  model + 1
+
+
+-- VIEW
+view: Model -> Element
+view model =
+  show model
 
 main: Signal Element
 main =
-  --Signal.map show keyPressedCount
-  Signal.map show mouseClickCount
+  --Signal.map show model
+  Signal.map view model
