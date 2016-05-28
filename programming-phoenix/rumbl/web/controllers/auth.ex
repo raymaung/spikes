d361c66430480eea353f3801899ca1ff8a1540d4
Raymond Maung <raymaung@gmail.com>
2016-05-28 14:10:57 +1000

    Add newly created user to session to set as logged

--- programming-phoenix/rumbl/web/controllers/auth.ex
+++ programming-phoenix/rumbl/web/controllers/auth.ex
@@ -16,4 +16,14 @@ defmodule Rumbl.Auth do
     # Adding :current_user to conn.assigns
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+      |> assign(:current_user, user)
+      |> put_session(:user_id, user.id)
+
+      # Tells plug to send session cookie back to the client with different
+      # identifiers
+      |> configure_session(renew: true)
+  end
 end
